a55307665136e5490ebb0d7926e0aff630012d14
JuanDArc0144 <chatabeto@gmail.com>
2026-02-22 16:25:06 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <body className='bg-[url(./images/bg.png)] bg-center w-screen h-screen '>
+      
+    </body>
+    )
+}
+export default App
