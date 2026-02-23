2756a9072fa164729518f96cd41973da8ddac531
JuanDArc0144 <chatabeto@gmail.com>
2026-02-23 11:24:37 -0600

    Second commit - Initialize project - [GLORY TO MANKIND].

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { Header } from './components/header'
 
 function App() {
 
   return (
-    <body className='bg-[url(./images/bg.png)] bg-center w-screen h-screen '>
+    <body className='bg-gray-900'>
+      <Header></Header>
       
     </body>
     )
---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+    return (
+        <>
+        <div className="flex">
+            <div className="bg-amber-400">
+            </div>
+        </div>
+        <div>
+
+        </div>
+        </>
+    )
+}
